3f2762a49508c3d026553984dc7029f19ae7af61
Omachi John <omachi.john@.com>
2022-10-31 20:44:15 +0100

    Correct errors pointed by reviewer

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 class Teacher < Person
-  def initialize(specialization)
-    super(name, age, parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 

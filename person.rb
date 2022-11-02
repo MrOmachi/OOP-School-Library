66bf20539ad6f92041f7caca10b372713e37f9ec
Omachi John <omachi.john@.com>
2022-11-02 20:36:11 +0100

    Add rental method to person.rb file

--- person.rb
+++ person.rb
@@ -10,6 +10,12 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rentals(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 
   def can_use_services?

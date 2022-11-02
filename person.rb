39530ca8a7546f455094b3318839667815cf0e11
Omachi John <omachi.john@.com>
2022-11-02 21:11:27 +0100

    Add rental method to person.rb file

--- person.rb
+++ person.rb
@@ -1,37 +1,37 @@
-require_relative './nameable'
+# require_relative './nameable'
 
-class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id
+# class Person < Nameable
+#   attr_accessor :name, :age
+#   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = Random.rand(1..50)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-    @rentals = []
-  end
+#   def initialize(age, name = 'Unknown', parent_permission: true)
+#     super()
+#     @id = Random.rand(1..50)
+#     @name = name
+#     @age = age
+#     @parent_permission = parent_permission
+#     @rentals = []
+#   end
 
-  def add_rentals(rental)
-    @rentals.push(rental)
-    rental.person = self
-  end
+#   def add_rentals(rental)
+#     @rentals.push(rental)
+#     rental.person = self
+#   end
 
-  def can_use_services?
-    of_age? || @parent_permission
-  end
+#   def can_use_services?
+#     of_age? || @parent_permission
+#   end
 
-  def correct_name
-    @name
-  end
+#   def correct_name
+#     @name
+#   end
 
-  private
+#   private
 
-  def of_age?
-    @age >= 18
-  end
-end
+#   def of_age?
+#     @age >= 18
+#   end
+# end
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
+# person = Person.new(22, 'maximilianus')
+# puts person.correct_name

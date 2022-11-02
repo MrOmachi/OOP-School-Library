23ee6c09f424c70a0a840b82b1dda6bcaa9279b3
Omachi John <omachi.john@.com>
2022-11-02 19:35:01 +0100

    Create classroom, book and rental files and classes

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,14 @@
-class Classroom
-  attr_accessor :label
-  attr_reader :atudents
+# class Classroom
+#   attr_accessor :label
+#   attr_reader :atudents
 
-  def initialize(label)
-    @label = label
-    @students = []
-  end
+#   def initialize(label)
+#     @label = label
+#     @students = []
+#   end
 
-  def add_student(student)
-    @students.push(student)
-    student.classroom = self
-  end
-end
+#   def add_student(student)
+#     @students.push(student)
+#     student.classroom = self
+#   end
+# end
